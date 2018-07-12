3ef15cfda0db38d38f83dfe625b8620684e260fc
Agustin <agustind@gmail.com>
2018-07-12 13:30:39 -0300

    updated header

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -12,9 +12,9 @@ const AppHeader = (props) => {
           <div className='app-left-nav'>
             <img src={logo} className='app-logo' alt='logo' />
             <div className='app-title-text'>
-              <h1 className='app-title'>Netlify + Fauna DB</h1>
+              <h1 className='app-title'>Serverless React app with Fauna DB Example</h1>
               <p className='app-intro'>
-                Using FaunaDB & Netlify functions
+
               </p>
             </div>
           </div>
